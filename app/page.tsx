ca209907005047216475e546905190e6c3b27df4
Jiayue Yu <z5448095@ad.unsw.edu.au>
2025-05-07 11:10:13 +1000

    Add formatting to the Home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <div className="flex flex-row border-2 gap-1">
-        <div>
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </div>
-        <input></input>
+    <div className="p-4 flex flex-col items-center">
+      <div className="w-full sm:w-5/6 flex flex-row items-center gap-2 border-2 border-gray-300 rounded-xl px-3 py-2 focus-within:ring-2 focus-within:ring-blue-500">
+        <i className="fa-solid fa-magnifying-glass text-gray-500"></i>
+        <input
+          className="w-full outline-none bg-transparent"
+          type="text"
+          placeholder="Search"
+        />
       </div>
-      <nav>
-        <div>Fiction</div>
-        <div>Non-fiction</div>
-        <div>Shop All</div>
+
+      <nav className="w-1/2 mt-4 flex flex-col sm:justify-around sm:flex-row">
+        <Link href={"/"} className="mx-auto hover:underline">
+          Fiction
+        </Link>
+        <Link href={"/"} className="mx-auto hover:underline">
+          Non-fiction
+        </Link>
+        <Link href={"/"} className="mx-auto hover:underline">
+          Shop All
+        </Link>
       </nav>
     </div>
   );
