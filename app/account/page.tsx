9d06abb8af49ff25e0f2ca782be68e831349e1ef
Jiayue Yu <z5448095@ad.unsw.edu.au>
2025-05-26 16:54:41 +1000

    Style the account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,16 +1,23 @@
 import { auth } from "@/auth";
 import { getPastOrders } from "../lib/data";
 import Image from "next/image";
+import { Fredoka } from "next/font/google";
+
+const fredoka = Fredoka({
+  subsets: ["latin"],
+  weight: ["300"],
+  display: "swap",
+});
 
 export default async function AccountPage() {
   const session = await auth();
   const pastOrders = await getPastOrders(session?.user.email || null);
   console.log(pastOrders);
   return (
-    <div>
+    <div className="m-5">
       <div>Hi {session?.user.name}</div>
       <div>
-        <h2>Past orders</h2>
+        <h2>Your Past Orders:</h2>
         <div>
           {pastOrders.map((order) => {
             return (
@@ -18,24 +25,35 @@ export default async function AccountPage() {
                 key={order.cartId}
                 className="border border-solid border-black p-4 mb-4"
               >
-                cartId: {order.cartId}
-                {order.items.map((item) => {
-                  return (
-                    <div key={item.bookId}>
-                      <Image
-                        src={`/bookCovers/${item.bookId}.png`}
-                        alt="book cover display"
-                        width={141}
-                        height={225}
-                      ></Image>
-                      <div>
-                        {item.title} by {item.author}
+                <h3>CartId: {order.cartId}</h3>
+                <h4 className={`${fredoka.className}`}>
+                  Created at {order.createDate.toDateString()}
+                </h4>
+                <div className="flex flex-row flex-wrap flex-1">
+                  {order.items.map((item) => {
+                    return (
+                      <div
+                        key={item.bookId}
+                        className="w-[150px] m-2 flex flex-col items-center"
+                      >
+                        <Image
+                          src={`/bookCovers/${item.bookId}.png`}
+                          alt="book cover display"
+                          width={141}
+                          height={225}
+                          className="object-cover"
+                        ></Image>
+                        <div>
+                          {item.title} by {item.author}
+                        </div>
+                        <div className="self-start text-sm">${item.price}</div>
                       </div>
-                      <div>${item.price}</div>
-                    </div>
-                  );
-                })}
-                <div>Total: ${order.totalPrice}</div>
+                    );
+                  })}
+                </div>
+                <div className="text-blue-500 font-bold">
+                  Total: ${order.totalPrice}
+                </div>
               </div>
             );
           })}
