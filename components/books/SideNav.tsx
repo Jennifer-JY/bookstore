8d46ba4d84315e0377e83d6e2cb81ed4d3b0c13d
Jiayue Yu <z5448095@ad.unsw.edu.au>
2025-06-02 15:02:41 +1000

    Adjust side nav bar spacing.

--- components/books/SideNav.tsx
+++ components/books/SideNav.tsx
@@ -3,7 +3,7 @@ import NavLinks from "./NavLinks";
 export default function SideNav() {
   return (
     <>
-      <div className="w-full inset-shadow-sm  border-gray-400 shadow-sm h-full">
+      <div className="w-full inset-shadow-sm  border-gray-400 shadow-sm h-full pl-4">
         <NavLinks />
       </div>
     </>
